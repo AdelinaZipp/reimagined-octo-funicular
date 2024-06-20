bd39d9db75a609d81271d1b6ad973a4a9b9998ae
AdelinaZipp <adelina.zipp@interia.pl>
2024-06-21 00:10:38 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 import { randomInt } from "crypto";
 import puppeteer from "puppeteer-extra";
-import axios from 'axios';
+import axios from "axios";
 import yes from "puppeteer-extra-plugin-stealth";
 let delay = (ms) => new Promise((r) => setTimeout(r, ms));
 puppeteer.default.use(yes());
 async function browser(user) {
-    await delay(randomInt(30000, 300000));
+    await delay(randomInt(1, 5));
     const browser = await puppeteer.default.launch({
         headless: false,
         args: ["--no-sandbox"],
@@ -15,12 +15,7 @@ async function browser(user) {
     let page = n[0];
     await page.goto("https://twitch.tv/");
     await delay(2000);
-
-    await delay(2000);
-    await (await page.$("#root > div > div.Layout-sc-1xcs6mc-0.lcpZLv > div.Layout-sc-1xcs6mc-0.gUvyVO > div > div > div > div.Layout-sc-1xcs6mc-0.fJANQ > div:nth-child(1) > button")).click();
-    await delay(500);
-
-        await page.setCookie({ name: "auth-token", value: token.data.thingy });
+    await page.setCookie({ name: "auth-token", value: token.data.thingy });
     console.log(":3");
     await page.goto("https://twitch.tv/");
     await delay(2000);
@@ -28,7 +23,14 @@ async function browser(user) {
     if (vv) {
         await page.reload();
     }
-    await delay(1000)
+    await delay(2000);
+    try {
+        await (await page.$("#root > div > div.Layout-sc-1xcs6mc-0.lcpZLv > div.Layout-sc-1xcs6mc-0.gUvyVO > div > div > div > div.Layout-sc-1xcs6mc-0.fJANQ > div:nth-child(1) > button"))
+            .click()
+            .catch((err) => err);
+    }
+    catch (err) { }
+    await delay(500);
     await (await page.$("#root > div > div.Layout-sc-1xcs6mc-0.lcpZLv > nav > div > div.Layout-sc-1xcs6mc-0.kuGBVB > div > div > div > div > div.Layout-sc-1xcs6mc-0.jNIlkd > div > div.ScInputWrapper-sc-1fxjr4l-0.drBdyB.tw-combo-input__input > div > div > input")).type(user);
     await delay(2500);
     const elements = await page.$$(`[id^="search-result-row"]`);
