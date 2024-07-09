24c591cba35682c5057c9f782aea51de72b13128
AdelinaZipp <adelina.zipp@interia.pl>
2024-07-09 22:54:48 +0200

    Update a.py

--- a.py
+++ a.py
@@ -36,7 +36,7 @@ def main():
     driver.add_cookie({'name' : 'auth-token', 'value' : sys.argv[1]})
     driver.refresh()
     driver.get('https://www.twitch.tv/'+sys.argv[2])
-    time.sleep(4)
+    time.sleep(9999999)
     driver.save_screenshot('sc3.png')
     # actions.send_keys("Tab")
     # actions.send_keys("Tab")
