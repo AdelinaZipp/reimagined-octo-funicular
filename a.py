0e3171a72a21ae72b99ca8cf5b4a54cc844a9a13
AdelinaZipp <adelina.zipp@interia.pl>
2024-07-09 19:03:31 +0200

    Update a.py

--- a.py
+++ a.py
@@ -6,41 +6,44 @@ from selenium.webdriver.remote.webdriver import By
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.keys import Keys
 
-
-chrome_options = Options()
-chrome_options.add_argument("--remote-debugging-port=9222")
-chrome_options.add_argument("--incognito")
-
-
-driver = uc.Chrome(headless=False,use_subprocess=False)
-
-time.sleep(5)
-driver.get('https://www.twitch.tv/')
-
-time.sleep(5)
-# driver.add_cookie({'name' : 'auth-token', 'value' : sys.argv[1]})
-driver.get('https://www.twitch.tv/')
-time.sleep(2)
-driver.save_screenshot('sc1.png')
-try:
-    # Code that might raise an exception
+def main():
+    chrome_options = Options()
+    chrome_options.add_argument("--remote-debugging-port=9222")
+    chrome_options.add_argument("--incognito")
 
 
-    testy = driver.find_element(By.CLASS_NAME, 'consent-banner')
-    testy.click()
-    actions = ActionChains(driver)
-    actions.send_keys(Keys.TAB)
-    actions.send_keys(Keys.TAB)
-    # actions.send_keys(Keys.TAB)
-    actions.send_keys(Keys.ENTER).perform()
-    print(testy)
-except:
-    print("An unknown error occurred.")
-time.sleep(4)
-driver.save_screenshot('sc2.png')
-# actions.send_keys("Tab")
-# actions.send_keys("Tab")
+    driver = uc.Chrome(headless=False,use_subprocess=False)
 
-time.sleep(5)
-driver.close()
+    time.sleep(5)
+    driver.get('https://www.twitch.tv/')
+
+    time.sleep(5)
+    # driver.add_cookie({'name' : 'auth-token', 'value' : sys.argv[1]})
+    driver.get('https://www.twitch.tv/')
+    time.sleep(2)
+    driver.save_screenshot('sc1.png')
+    try:
+        # Code that might raise an exception
+        # /html/body/div[1]/div/div[1]/div[1]/div/div/div/div[3]/button
+        # #root > div > div.Layout-sc-1xcs6mc-0.lcpZLv > div.Layout-sc-1xcs6mc-0.gUvyVO > div > div > div > div.Layout-sc-1xcs6mc-0.joANJJ > button
+
+        testy = driver.find_element(By.CLASS_NAME, 'consent-banner')
+        testy.click()
+        actions = ActionChains(driver)
+        actions.send_keys(Keys.TAB)
+        actions.send_keys(Keys.TAB)
+        # actions.send_keys(Keys.TAB)
+        actions.send_keys(Keys.ENTER).perform()
+        print(testy)
+    except:
+        print("An unknown error occurred.")
+    time.sleep(4)
+    driver.save_screenshot('sc2.png')
+    # actions.send_keys("Tab")
+    # actions.send_keys("Tab")
+
+    time.sleep(5)
+    driver.close()
 # driver.save_screenshot('nowsecure.png')
+if __name__ == "__main__":
+    main()
