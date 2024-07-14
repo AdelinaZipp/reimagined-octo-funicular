50226ba425dad867e245f1bf7dd9a75c4fcabae0
AdelinaZipp <adelina.zipp@interia.pl>
2024-07-14 03:45:12 +0200

    Update vv.js

--- vv.js
+++ vv.js
@@ -7,7 +7,7 @@ let delay = (ms) => new Promise((r) => setTimeout(r, ms));
 async function uwu(){
     let arr = []
     for(let n=0;n>5;n++){
-        arr.push(execAsync(`xvfb-run -n ${n+20} https://www.twitch.tv/foxyman361`))
+        arr.push(execAsync(`xvfb-run -n ${n+20} chromium https://www.twitch.tv/foxyman361`))
         await delay(25000)
     }
     console.log(":3")
